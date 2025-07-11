e32618e970fd4f85bcf211e2a8b5d08b7dce2776
sQu1rr <sQu1rr1c@gmail.com>
2025-07-11 16:36:06 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/navigationexperiment/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.navigationexperiment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val args = HelloWorldFragmentArgs.fromSavedStateHandle(SavedStateHandle())
+        Log.d("MainActivity", "ARG: ${args.arg}")
+
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
